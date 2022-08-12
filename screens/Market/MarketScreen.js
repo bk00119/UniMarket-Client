7b3a0617529ac4c6a3543435942e2e7ee749b81b
Kevin Ng <80858693+kn2118@users.noreply.github.com>
2022-08-12 18:57:54 -0400

    AppNavigationSetup

---
+++ screens/Market/MarketScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+
+function MarketScreen(props) {
+    return (
+        <View>
+            <Text>MarketScreen</Text>
+        </View>
+    );
+}
+
+export default MarketScreen;
